d46871239d81378c8154d22450c4a154ec54279c
kfac <kfacison1998@gmail.com>
2023-10-24 15:19:08 -0500

    add token

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const token = process.env.GITHUB_TOKEN;
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const username = req.query.username;
+  console.log(`username: ${username}`);
+  res.render('index');
 });
 
 module.exports = router;
